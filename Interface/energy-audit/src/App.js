76d50fe4816dcaf1815d18a4b3cac0aa95e6ad21
Andrew J. Buist <andrew.jbuist@gmail.com>
2019-10-26 14:54:14 -0400

    Added links to the header so we can traverse pages

--- Interface/energy-audit/src/App.js
+++ Interface/energy-audit/src/App.js
@@ -11,9 +11,16 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <Router>
+    {/*The nav bar won't contain links to all these pages later, this is temporary to get to each page*/}
     <nav>
       <Link to="/" style={{ color: "white" }}>
         <h3>Energy Audit</h3>
+      </Link>
+      <Link to="/picture" style={{ color: "white" }}>
+        <h3>Picture Page</h3>
+      </Link>
+      <Link to="/configuration" style={{ color: "white" }}>
+        <h3>Config Page</h3>
       </Link>
     </nav>
     <Route path="/" exact component={Home} />
