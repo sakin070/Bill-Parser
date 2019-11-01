216955f017dc1187f1010a45a98f2f99dfe5ea53
Saheed Akinbile <sakin070@uottawa.ca>
2019-11-01 10:48:08 -0400

    added working unit tests and new methods to improve code readability and atomicity of some methods

--- billToText.py
+++ billToText.py
@@ -11,7 +11,7 @@ tempProcessingLocation = "./imageOutputFolder"
 # testImage has no alpha, so when processing documents, remember that we don't want alpha
 # create an array for a list of supported bill templates
 
-def parseTuple(tpl):
+def parseConfigItems(tpl):
     tpl = tpl[1:-1]
     # you want to put a try catch to prevent user error
     lst = tpl.split(', ')
@@ -27,18 +27,28 @@ def pdfToImages(filePath,name='image'):
 def extractInfo(imagePath):
     # check if its an object or a path to an object
     # current implementation is of a path
-    config = configparser.RawConfigParser()
-    config.read('config.cfg')
-    items = config.items('Hydro Ottawa')
+
     imageInfo = {}
     image = Image.open(imagePath)
+    items = readConfig()
     for item in items:
-        imageSection = image.crop(parseTuple(item[1]))
+        imageSection = image.crop(parseConfigItems(item[1]))
         imageInfo[item[0]] = str(tesserocr.image_to_text(imageSection)).rstrip()
+    # return imageInfo
     return dictionaryToJson(imageInfo)
+
 
 def dictionaryToJson(dic):
     json_data = json.dumps(dic)
     return json_data
 
-print(extractInfo("./resources/testImg.png"))
+
+def readConfig(configFile = 'config.cfg', selection='Hydro Ottawa'):
+    config = configparser.ConfigParser()
+    config.read(configFile)
+    items = config.items(selection)
+    return items
+
+# print(extractInfo("./resources/testImg.png"))
+
+# readConfig()
