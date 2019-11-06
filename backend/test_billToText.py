bbbf56d8751cef3927bfd71de342026c22aac171
Matteo Pimentel <ubermatteo391@gmail.com>
2019-11-05 20:58:31 -0500

    added testPdfConvert for pdf convert

--- backend/test_billToText.py
+++ backend/test_billToText.py
@@ -1,5 +1,6 @@
 import unittest
 import billToText as bt
+import os
 
 class TestBillToTextMethods(unittest.TestCase):
 
@@ -23,6 +24,17 @@ class TestBillToTextMethods(unittest.TestCase):
         expected = [('date', '(260, 674, 406, 720)'), ('usage', '(301, 386, 400, 432)'), ('cost', '(1104, 675, 1224, 720)')]
         actual = bt.readConfig(selection='Hydro Ottawa')
         self.assertEqual(actual, expected)
+    
+    #TODO: Code Review
+    def testPdfConvert(self):
+        if os.path.exists("backend/resources/image.jpg"):
+            os.remove("backend/resources/image.jpg")
+        pdfToImages("backend/resources/testPDFOttawa.jpg")
+        self.assertTrue(os.path.exists("backend/resources/image.jpg"))
+        
+        
+        
+        
 
 
 if __name__ == '__main__':
