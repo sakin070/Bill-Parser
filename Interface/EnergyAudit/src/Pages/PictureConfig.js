0b897c1de52c84ae5f429c64e18ade2e80b755bc
Andrew Buist <andrew.jbuist@gmail.com>
2019-11-05 20:37:15 -0500

    Created Router/temp header

--- Interface/EnergyAudit/src/Pages/PictureConfig.js
+++ Interface/EnergyAudit/src/Pages/PictureConfig.js
@@ -1,26 +1,25 @@
-import React from "react"
+import React from "react";
 import SelectionCanvas from "./util/SelectionCanvas";
 import ConfigTable from "./util/ConfigTable";
 
-class PictureConfig extends React.Component{
-    constructor(props){
-        super(props);
-    }
+class PictureConfig extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return(
-            <div className="container">
-                <div className="row">
-                    <div className="col col-lg-9">
-                       <SelectionCanvas/>
-                    </div>
-                    <div className="col col-lg-3">
-                       <ConfigTable/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col col-lg-9">
+            <SelectionCanvas />
+          </div>
+          <div className="col col-lg-3">
+            <ConfigTable />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-export default PictureConfig
-
+export default PictureConfig;
